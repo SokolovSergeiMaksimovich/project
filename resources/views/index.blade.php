d8113a7a94fbf1f97dccebfedc0e0eb14216611a
Sokolov Sergei <SokolovSergeiMaksimovich@gmail.com>
2023-04-11 21:23:21 +0600

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@extends('module.layout')
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>Document</title>
+</head>
+<body class="container mt-5">
+    <a href="{{route('module.index')}}" class="btn btn-primary w-100">Модуль</a>
+</body>
+</html>
